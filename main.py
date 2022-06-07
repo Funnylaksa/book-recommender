ff2e36a07b15eacc1aa61b734e0f0943137106eb
Funnylaksa <ivan.lee.ch8@gmail.com>
2022-06-07 11:06:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-import numpy as np
 import pandas as pd
 from flask import Flask, render_template, request
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
-import json
-import bs4 as bs
-import urllib.request
-import pickle
-import re
-import requests
-from datetime import date, datetime
 
 # load the nlp model and tfidf vectorizer from disk
 # filename = 'nlp_model.pkl'
