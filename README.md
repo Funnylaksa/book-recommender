2fd1f733eb28146a29e7c172a0f0c22f704005b6
Funnylaksa <ivan.lee.ch8@gmail.com>
2022-06-15 00:22:58 +0800

    Add explainability for each input

--- README.md
+++ README.md
@@ -3,34 +3,37 @@
 The books dataset is taken from https://github.com/malcolmosh/goodbooks-10k-extended/blob/master/README.md. The dataset contains ten thousand most popular books with over six million ratings.
 
 ## Overview
-There are 3 inputs to this application. 
+There are 3 inputs in this web app. 
 1) User ID
 2) Genre
 3) Book 
 
-Based on the inputs, a list of recommended books will be returned using different recommendation models. The default list is set to a 100 books and can be lower depending on the inputs. 
+Based on the inputs, a list of recommended books will be generated using different models. The default list is set to a 100 books and can be lower depending on the inputs. 
 
 
 ## Finding similar books (Collaborative Filtering)
-Using Collaborative Deep Learning (CDL), a recommender model is created based on the user ratings. This aim is to recommend based on collaborative filtering, to recommend user books based on what similar users rate highly. On top of ratings data, books description is also fed into the model, providing an additional dimension to boost the recommender using text modality. 
+The 1st recommender model is created using Collaborative Deep Learning (CDL) training on user ratings. This model uses collaborative filtering, and aims to recommend user books based on what similar users rate highly. On top of ratings data, books description is also fed into the model, providing an additional dimension(text modality) to boost the performance of the recommender.
 
-From the input dataset, user preferences data available in the form of ratings for books provided by a set of users. Using the preference data, CDL can be used to generate a list of recommended books based on what similar users like. 
+From the input dataset, user preference data is available in the form of book ratings given by a group of users. Using the preference data, CDL can be used to generate a list of recommended books based on what similar users like. 
 
-When a book input is given, the model can also predict a list of other books which are like the given book based on the latent factors from CDL.
+When a book input is given, the model can also predict a list of other books which are similar to given book using the latent factors generated from CDL.
 
 
 ## Content Based Filtering
 When a genre input is given, the model will return a list of most popular books in the selected genre. Popularity is calculated using the true Bayesian estimate, where a score is given using both the average ratings and rating counts. Even if a rating of a book is high, it might rank lower on this estimate score system if it has low rating counts.
 
-When no inputs are given, which simulates a cold start where no information is available about the new user, the most popular 100 books are returned as a result. From there, the user can select the books and find similar books to their selection.
+
+## Cold Start
+When no inputs are given, which simulates a cold start problem where no information is available about the new user, the most popular 100 books are returned as a result. From there, the user can select the books or choose a gerne to find books that suits their taste.
  
  
 ## Multiple inputs
-When multiple inputs are given, recommended list of books are generated for each input. Books that appear in multiple recommendations will be placed at the top, with the remaining choices added to list in a round robin fashion.
+When multiple inputs are fed into the system, recommended list of books are generated for each input. The algorithm will display the books that matches all inputs at the top as "Top Recommendations". Remaining books will be displayed based on each inputs in different sections.
 
 
 ## Future Works
-In a real-world scenario, book similarity can be calculated in real time based on users’ input. For personalised recommendation, CDL can be regenerated on a weekly basis to take in data of new users and new ratings from existing users to build up a stronger recommender. 
+In a real-world scenario, book similarity can be calculated in real time based on users’ input. For personalised recommendation though, CDL will need to be retrained on a routinely basis based on business needs to take in data of new users and new ratings to build up a stronger model.
+An additional feature will be needed to collect user preference for the books they have seen, so the new data can be used to update the model. The feature can be a thumbs up, thumbs down button, or a rating out of 5 for more precise scoring.  
 
 
 
--- main.py
+++ main.py
@@ -28,14 +28,36 @@ def escape_special_char(my_string):
         .replace("[", "\[").replace("]", "\]").replace("*", "\*").replace("$", "\$")
 
 
-def combine(lst1, lst2):
-    similar = list(set(lst1) & set(lst2))
-    remain1 = [i for i in lst1 if i not in similar]
-    remain2 = [i for i in lst2 if i not in similar]
-    for i in range(100 - len(similar)):
-        similar.append(remain1[i])
-        similar.append(remain2[i])
-    return similar[:100]
+def combine(lst1, lst2, lst3=False):
+    if lst3:
+        similar = list(set(lst1) & set(lst2) & set(lst3))
+        remain_count = (50 - len(similar)) // 3
+        remain1 = [i for i in lst1 if i not in similar][:remain_count]
+        remain2 = [i for i in lst2 if i not in similar][:remain_count]
+        remain3 = [i for i in lst3 if i not in similar][:remain_count]
+        return similar, remain1, remain2, remain3
+    else:
+        similar = list(set(lst1) & set(lst2))
+        remain_count = (50 - len(similar)) // 2
+        remain1 = [i for i in lst1 if i not in similar][:remain_count]
+        remain2 = [i for i in lst2 if i not in similar][:remain_count]
+        return similar, remain1, remain2
+
+
+def get_recommended_list(lst):
+    if not lst:
+        return []
+    df_rec = pd.DataFrame()
+    for id in lst:
+        df_rec = df_rec.append(df[df.book_id == id])
+    rec_posters = list(df_rec["image_url"])
+    rec_books = list(df_rec["original_title"])
+    rec_vote = list(df_rec["average_rating"])
+    rec_year = [int(i) for i in list(df_rec["original_publication_year"])]
+    rec_books_org = rec_books
+
+    return {rec_posters[i]: [rec_books[i], rec_books_org[i], rec_vote[i], rec_year[i]] for i in
+            range(len(rec_posters))}
 
 
 app = Flask(__name__)
@@ -51,12 +73,14 @@ def home():
 @app.route("/recommend", methods=["POST"])
 def recommend():
     # Get input from Web App FrontEnd
+    k = 50
     title = escape_special_char(request.form['title'])
     user_id = request.form['user']
     genre = request.form['genre']
     print(f"title: {title}")
     print(f"user_id: {user_id}")
     print(f"genre: {genre}")
+    book_lst = user_lst = genre_lst = combine_lst = pop_lst = []
 
     # If book chosen, display info on book
     ser = df[df["original_title"].str.contains(title, case=False)]
@@ -73,71 +97,65 @@ def recommend():
         book_id = df[df["original_title"].str.contains(title, case=False)].book_id.values[0]
         book_mask = df_recommend_by_book.loc[[book_id]].recommended_books.values[0]
         user_mask = df_recommend_by_user.loc[[int(user_id)]].recommended_books.values[0]
-        genre_mask = list(most_pop[most_pop.genres.str.contains(genre)].book_id)
-        book_and_user = combine(book_mask, user_mask)
-        rec_list = list(set(book_and_user) & set(genre_mask))
+        genre_mask = list(most_pop[most_pop.genres.str.contains(genre)].book_id)[:50]
+        combine_lst, book_lst, user_lst, genre_lst = combine(book_mask, user_mask, genre_mask)
         print("recommend based on title, user & genre")
 
     elif title != "" and user_id != "":
         book_id = df[df["original_title"].str.contains(title, case=False)].book_id.values[0]
         book_mask = df_recommend_by_book.loc[[book_id]].recommended_books.values[0]
         user_mask = df_recommend_by_user.loc[[int(user_id)]].recommended_books.values[0]
-        rec_list = combine(book_mask, user_mask)
+        combine_lst, book_lst, user_lst = combine(book_mask, user_mask)
         print("recommend based on title & user")
 
     elif title != "" and genre != "":
         book_id = df[df["original_title"].str.contains(title, case=False)].book_id.values[0]
         book_mask = df_recommend_by_book.loc[[book_id]].recommended_books.values[0]
-        genre_mask = list(most_pop[most_pop.genres.str.contains(genre)].book_id)
-        rec_list = list(set(book_mask) & set(genre_mask))
+        genre_mask = list(most_pop[most_pop.genres.str.contains(genre)].book_id)[:50]
+        combine_lst, genre_lst, book_lst = combine(genre_mask, book_mask)
         print("recommend based on title & genre")
 
     elif user_id != "" and genre != "":
         user_mask = df_recommend_by_user.loc[[int(user_id)]].recommended_books.values[0]
-        genre_mask = list(most_pop[most_pop.genres.str.contains(genre)].book_id)
-        rec_list = list(set(user_mask) & set(genre_mask))
+        genre_mask = list(most_pop[most_pop.genres.str.contains(genre)].book_id)[:50]
+        combine_lst, genre_lst, user_lst = combine(genre_mask, user_mask)
         print("recommend based on user & genre")
 
     elif title != "":
         book_id = df[df["original_title"].str.contains(title, case=False)].book_id.values[0]
-        rec_list = df_recommend_by_book.loc[[book_id]].recommended_books.values[0]
+        book_lst = df_recommend_by_book.loc[[book_id]].recommended_books.values[0]
         print("recommend based on title")
 
     elif user_id != "":
-        rec_list = df_recommend_by_user.loc[[int(user_id)]].recommended_books.values[0]
+        user_lst = df_recommend_by_user.loc[[int(user_id)]].recommended_books.values[0]
         print("recommend based on user")
 
     elif genre != "":
-        rec_list = list(most_pop[most_pop.genres.str.contains(genre)].head(100).book_id)
+        genre_lst = list(most_pop[most_pop.genres.str.contains(genre)].head(50).book_id)
         print("recommend based on genre")
 
     else:
-        rec_list = list(most_pop.book_id)[:100]
+        pop_lst = list(most_pop.book_id)[:50]
         print("recommend based on most popular")
 
-    print(f"rec list: {rec_list}")
-    df_rec = pd.DataFrame()
-    for id in rec_list:
-        df_rec = df_rec.append(df[df.book_id == id])
+    print(f"combine list: {combine_lst}")
+    print(f"genre list: {genre_lst}")
+    print(f"book list: {book_lst}")
+    print(f"user list: {user_lst}")
 
-    if rec_list:
-        # Details of recommended items
-        rec_posters = list(df_rec["image_url"])
-        rec_books = list(df_rec["original_title"])
-        rec_vote = list(df_rec["average_rating"])
-        rec_year = [int(i) for i in list(df_rec["original_publication_year"])]
-        rec_books_org = rec_books
-
-        book_cards = {rec_posters[i]: [rec_books[i], rec_books_org[i], rec_vote[i], rec_year[i]] for i in
-                      range(len(rec_posters))}
-    else:
-        book_cards = []
-        print('no recommendation based on selection')
+    # if combine_lst:
+    book_cards = get_recommended_list(combine_lst)
+    g_book_cards = get_recommended_list(genre_lst)
+    b_book_cards = get_recommended_list(book_lst)
+    u_book_cards = get_recommended_list(user_lst)
+    p_book_cards = get_recommended_list(pop_lst)
 
     # passing all the data to the html file
     return render_template('recommend.html', title=title, vote_average=vote_average,
                            vote_count=vote_count, release_date=release_date, authors=authors, user_id=user_id,
-                           poster=poster, book_cards=book_cards, overview=overview, genres=genres, genre=genre)
+                           poster=poster, book_cards=book_cards, overview=overview, genres=genres, genre=genre,
+                           b_book_cards=b_book_cards, u_book_cards=u_book_cards, g_book_cards=g_book_cards,
+                           p_book_cards=p_book_cards)
 
 
 if __name__ == '__main__':
