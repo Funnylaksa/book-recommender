da403864051e0c3c7bc3085dda0d367cf4e27653
Funnylaksa <ivan.lee.ch8@gmail.com>
2022-06-21 15:53:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from typing import Any
 
 import pandas as pd
 from flask import Flask, render_template, request
-import random
 from ast import literal_eval
 from collections import OrderedDict
 
@@ -29,6 +28,15 @@ def escape_special_char(my_string):
 
 
 def combine(lst1, lst2, lst3=False):
+    """
+    If 2 inputs given, return 3 lists
+    1) Similar recommendation in both list
+    2) recommendation based on input 1
+    3) recommendation based on input 2
+
+    Similar concept for 3 inputs
+    """
+
     if lst3:
         similar = list(set(lst1) & set(lst2) & set(lst3))
         remain_count = (50 - len(similar)) // 3
@@ -77,9 +85,6 @@ def recommend():
     title = escape_special_char(request.form['title'])
     user_id = request.form['user']
     genre = request.form['genre']
-    print(f"title: {title}")
-    print(f"user_id: {user_id}")
-    print(f"genre: {genre}")
     book_lst = user_lst = genre_lst = combine_lst = pop_lst = []
 
     # If book chosen, display info on book
@@ -94,56 +99,50 @@ def recommend():
 
     # Recommended Items based on inputs
     if title != "" and user_id != "" and genre != "":
+        # recommend based on title, user & genre
         book_id = df[df["original_title"].str.contains(title, case=False)].book_id.values[0]
         book_mask = df_recommend_by_book.loc[[book_id]].recommended_books.values[0]
         user_mask = df_recommend_by_user.loc[[int(user_id)]].recommended_books.values[0]
         genre_mask = list(most_pop[most_pop.genres.str.contains(genre)].book_id)[:50]
         combine_lst, book_lst, user_lst, genre_lst = combine(book_mask, user_mask, genre_mask)
-        print("recommend based on title, user & genre")
 
     elif title != "" and user_id != "":
+        # recommend based on title & user
         book_id = df[df["original_title"].str.contains(title, case=False)].book_id.values[0]
         book_mask = df_recommend_by_book.loc[[book_id]].recommended_books.values[0]
         user_mask = df_recommend_by_user.loc[[int(user_id)]].recommended_books.values[0]
         combine_lst, book_lst, user_lst = combine(book_mask, user_mask)
-        print("recommend based on title & user")
 
     elif title != "" and genre != "":
+        # recommend based on title & genre
         book_id = df[df["original_title"].str.contains(title, case=False)].book_id.values[0]
         book_mask = df_recommend_by_book.loc[[book_id]].recommended_books.values[0]
         genre_mask = list(most_pop[most_pop.genres.str.contains(genre)].book_id)[:50]
         combine_lst, genre_lst, book_lst = combine(genre_mask, book_mask)
-        print("recommend based on title & genre")
 
     elif user_id != "" and genre != "":
+        # recommend based on user & genre
         user_mask = df_recommend_by_user.loc[[int(user_id)]].recommended_books.values[0]
         genre_mask = list(most_pop[most_pop.genres.str.contains(genre)].book_id)[:50]
         combine_lst, genre_lst, user_lst = combine(genre_mask, user_mask)
-        print("recommend based on user & genre")
 
     elif title != "":
+        # recommend based on title
         book_id = df[df["original_title"].str.contains(title, case=False)].book_id.values[0]
         book_lst = df_recommend_by_book.loc[[book_id]].recommended_books.values[0]
-        print("recommend based on title")
 
     elif user_id != "":
+        # recommend based on user
         user_lst = df_recommend_by_user.loc[[int(user_id)]].recommended_books.values[0]
-        print("recommend based on user")
 
     elif genre != "":
+        # recommend based on genre
         genre_lst = list(most_pop[most_pop.genres.str.contains(genre)].head(50).book_id)
-        print("recommend based on genre")
 
     else:
+        # recommend based on most popular
         pop_lst = list(most_pop.book_id)[:50]
-        print("recommend based on most popular")
 
-    print(f"combine list: {combine_lst}")
-    print(f"genre list: {genre_lst}")
-    print(f"book list: {book_lst}")
-    print(f"user list: {user_lst}")
-
-    # if combine_lst:
     book_cards = get_recommended_list(combine_lst)
     g_book_cards = get_recommended_list(genre_lst)
     b_book_cards = get_recommended_list(book_lst)
